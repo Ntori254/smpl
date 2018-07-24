80b85fe53a36ca59b66e5b055d351bd35e3a4087
emintori <ntori.emi@gmail.com>
2018-07-24 12:47:20 +0300

    added three new files fibo.py, fibotest.py and functions.py

---
+++ fibotest.py
@@ -0,0 +1,3 @@
+import fibo
+fibo.fib1(100)
+fibo.fib2(1000)
---
+++ functions2.py
@@ -0,0 +1,5 @@
+def message():
+    Texts="the quick brown fox jumps over the lazy dog"
+    for Mess in Texts:
+        print(Mess.title())
+print(message())
