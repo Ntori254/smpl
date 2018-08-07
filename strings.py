e57433321d94419f57310c46cb681b4bfae161b5
emintori <ntori.emi@gmail.com>
2018-08-07 11:51:38 +0300

    corrected the error where there is an additional tuple that shouldn't be there

--- strings.py
+++ strings.py
@@ -3,4 +3,4 @@ middleName='Ntori'
 lastName= 'Onyoni'
 fullName='Emi N. Onyoni'
 
-print("My First name is {0}, my middle name is {1} and my last name is {3}. My full name is {4}")
+print("My First name is {0}, my middle name is {1} and my last name is {2}. My full name is {3} ".format(firstName,middleName,lastName,fullName))
