f2b799d3a300fa60e963b19fb9355e5f2ae5bffa
emintori <ntori.emi@gmail.com>
2018-08-07 11:44:47 +0300

    use string to display my name

---
+++ strings.py
@@ -0,0 +1,6 @@
+firstName='Emi'
+middleName='Ntori'
+lastName= 'Onyoni'
+fullName='Emi N. Onyoni'
+
+print("My First name is {0}, my middle name is {1} and my last name is {3}. My full name is {4}")
